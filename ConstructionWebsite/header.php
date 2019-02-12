544ab9192c385f1ecc5385548fda7abd23350a0b
Harrison Elliott (Student) <eh307350@callywith.ac.uk>
2019-02-12 14:31:07 +0000

    Added Construction Website + HTML Updates

---
+++ ConstructionWebsite/header.php
@@ -0,0 +1,52 @@
+<div class="smallHeader container-fluid">
+    <div class="row smHeader">
+        <div class="col-sm-5">
+            <p>Some Address, Some Town, Cornwall</p>
+        </div>
+        <div class="col-sm-2">
+            <p><a href="mailto:sales@somemail.com">sales@someemail.com</a></p>
+        </div>
+
+        <div class="col-sm-1"></div>
+        <div class="col-sm-4">
+        <div class="row">
+            <div class="col-sm-11"><p>07432 655962</p></div>
+        </div>
+        </div>
+    </div>
+</div>
+<div class="largeHeader container-fluid sticky-top">
+    <div class="row lgHeader">
+        <div class="col-sm-4">
+            <h1>Name / Logo</h1>
+        </div>
+        <div class="col-sm-4">
+        </div>
+        <div class="col-sm-4">
+            <nav class="navbar navbar-expand-lg navbar-light">
+                <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                    <span class="navbar-toggler-icon"></span>
+                </button>
+                <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                    <ul class="navbar-nav mr-auto">
+                    <li class="nav-item active">
+                            <a class="nav-link" href="./index.php">Home</a>
+                        </li>
+                        <li class="nav-item <?php if($currentPage=="about") ?>">
+                            <a class="nav-link" href="#">About Us</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="#">Portfolio</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="#">Testomonals</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="#">Contact Us</a>
+                        </li>
+                    </ul>
+                </div>
+            </nav>
+        </div>
+    </div>
+</div>
